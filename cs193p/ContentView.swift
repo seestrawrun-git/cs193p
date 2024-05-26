7d4c8bb20fb3640915d53d670a653750e6f44743
Christopher Strawley <css9729@nyu.edu>
2024-05-26 16:41:51 -0400

    Added CardView to ContentView.

--- cs193p/ContentView.swift
+++ cs193p/ContentView.swift
@@ -9,16 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
+
+        .imageScale(.small)
         .padding()
+        .foregroundColor(.orange)
     }
 }
 
-#Preview {
-    ContentView()
+struct CardView: View {
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("😅")
+                    .font(.largeTitle)
+            } else {
+                base.fill()
+                    
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
