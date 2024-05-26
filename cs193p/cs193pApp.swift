42f27bd0e3f01c3067e6715af78374bd1e26397e
Christopher Strawley <css9729@nyu.edu>
2024-05-26 15:16:31 -0400

    Initial Commit

---
+++ cs193p/cs193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193pApp.swift
+//  cs193p
+//
+//  Created by Christopher Strawley on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct cs193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
