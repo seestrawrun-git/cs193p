043db43d1e8c0d7873190e648ff6673fb00fdd45
Christopher Strawley <css9729@nyu.edu>
2024-05-26 20:30:23 -0400

    Creating Functions for making card adders or remover buttons to simplify the var body of the main Content View

--- cs193p/ContentView.swift
+++ cs193p/ContentView.swift
@@ -8,20 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let emojis: [String] = ["😅", "😩", "🫡", "👄", "☝️", "😅", "😩", "🫡", "👄", "☝️"]
+    @State var cardCount: Int = 4
+    
     var body: some View {
-        HStack{
-            let emojis: [String] = ["😅", "😩", "🫡", "👄", "👄"]
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index])
-
-            }
-
+        VStack {
+            cards
+            cardCountAdjuster
         }
-
-        .imageScale(.small)
         .padding()
-        .foregroundColor(.orange)
     }
+    
+    
+    
+    var cardCountAdjuster: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+        
+        
+    }
+    
+    var cards: some View {
+            HStack{
+                ForEach(0..<cardCount, id: \.self) { index in
+                    CardView(content: emojis[index])}
+            }
+            .foregroundColor(.orange)
+        
+    }
+    
+    func cardCountAdjustment(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount+=offset
+        }, label: {
+            Image(systemName: symbol)
+        }
+            )
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+        
+    }
+    
+    
+    var cardRemover: some View {
+        return cardCountAdjustment(by: -1, symbol: "rectangle.stack.badge.minus")
+    }
+    
+    var cardAdder: some View {
+        return cardCountAdjustment(by: 1, symbol: "rectangle.stack.badge.plus")
+    }
+        
+ 
 }
 
 struct CardView: View {
